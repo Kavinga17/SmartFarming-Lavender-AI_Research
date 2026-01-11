5fc51686b672d2564ef1b26fa4ecebddba19223c
Sayumi Fernando <159319094+sayufernando@users.noreply.github.com>
2026-01-11 19:23:26 +0530

    Delete lavender_ai_app directory

--- lavender_ai_app/README.md
+++
@@ -1,7 +0,0 @@
-# lavender_ai_app
-
-Flutter frontend for soil health management
-
-## Getting Started
-
-contains the main dashboard
--- lavender_ai_app/android/app/src/main/kotlin/com/example/lavender_ai_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lavender_ai_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
